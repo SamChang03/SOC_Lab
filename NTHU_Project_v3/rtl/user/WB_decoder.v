87e7755c4f59bc7231aafef08480ec0d1c12e55e
Sam Chang <136160291+SamChang03@users.noreply.github.com>
2024-05-06 16:28:33 +0800

    Update WB_decoder.v

--- NTHU_Project_v3/rtl/user/WB_decoder.v
+++ NTHU_Project_v3/rtl/user/WB_decoder.v
@@ -1,43 +1,7 @@
-// SPDX-FileCopyrightText: 2020 Efabless Corporation
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// SPDX-License-Identifier: Apache-2.0
-
-//`default_nettype none // modified by us
 `default_nettype wire // modified by us
 `define MPRJ_IO_PADS_1 19	/* number of user GPIO pads on user1 side */ // modified by us
 `define MPRJ_IO_PADS_2 19	/* number of user GPIO pads on user2 side */ // modified by us
 `define MPRJ_IO_PADS (`MPRJ_IO_PADS_1 + `MPRJ_IO_PADS_2) // modified by us
-/*
- *-------------------------------------------------------------
- *
- * user_proj_example
- *
- * This is an example of a (trivially simple) user project,
- * showing how the user project can connect to the logic
- * analyzer, the wishbone bus, and the I/O pads.
- *
- * This project generates an integer count, which is output
- * on the user area GPIO pads (digital output only).  The
- * wishbone connection allows the project to be controlled
- * (start and stop) from the management SoC program.
- *
- * See the testbenches in directory "mprj_counter" for the
- * example programs that drive this user project.  The three
- * testbenches are "io_ports", "la_test1", and "la_test2".
- *
- *-------------------------------------------------------------
- */
 
 /////module user_proj_example #(
 module WB_decoder #(
@@ -75,15 +39,7 @@ module WB_decoder #(
 
     // IRQ
     output [2:0] irq
-);
-    
-    ///////////////////////////////////////// (For test) /////////////////////////////////////////
-    wire test2;
-
-    assign test2=(wbs_adr_i==32'h38000218);
-
-    //////////////////////////////////////////////////////////////////////////////////////////////
-    
+); 
     
     
     wire clk;
@@ -438,5 +394,3 @@ module WB_decoder #(
 
 
 endmodule
-
-`default_nettype wire
